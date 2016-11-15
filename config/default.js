65733fd586fb9a906ff4e039685f8bab28ac0133
ssarcandy <ssarcandy@gmail.com>
2016-11-16 01:38:21 +0800

    'init'

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    user_email: "",
+    user_password: "",
+    notify_channel_id: 'fb_user_id',
+    filter_list:[
+        'cml5',
+        'cml15',
+    ]
+};
---
+++ index.js
@@ -0,0 +1,32 @@
+const config = require('./config/config');
+const cheerio = require('cheerio');
+const request = require('request');
+const login = require('facebook-chat-api');
+
+let deadMachines = [];
+
+login({
+    email: config.user_email,
+    password: config.user_password
+}, function (err, api) {
+    if (err) return console.error(err);
+
+    request('http://www.cmlab.csie.ntu.edu.tw/status/', function (err, res, body) {
+        const $ = cheerio.load(body);
+        const dead = $('.dead');
+
+        for (let i = 0; i < dead.length; i++) {
+            const machineId = dead.eq(i).text();
+            if (~config.filter_list.indexOf(machineId)) continue;
+
+            deadMachines.push(machineId);
+        }
+
+        // all machines are fine~
+        if (!deadMachines.length) return;
+
+        api.sendMessage(`${deadMachines.toString()} is dead.`, config.notify_channel_id, function (err) {
+            console.log(`${deadMachines.toString()} is dead.`);
+        });
+    });
+});

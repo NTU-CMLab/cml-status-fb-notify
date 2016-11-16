c91d47d3aa1ecb45b961b25bf6c604bb638c9471
ssarcandy <ssarcandy@gmail.com>
2016-11-16 21:35:50 +0800

    get screenshot

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const config = require('./config/config');
 const cheerio = require('cheerio');
 const request = require('request');
 const login = require('facebook-chat-api');
+const webshot = require('webshot');
+const fs = require('fs');
 
 let deadMachines = [];
+const css = 'body{background:white;}.low{background-color:#ff0}.medium{background-color:#F93}.high{background-color:#F33}.dead{background-color:red}';
 
 request('http://www.cmlab.csie.ntu.edu.tw/status/', function (err, res, body) {
     const $ = cheerio.load(body);
@@ -18,18 +21,32 @@ request('http://www.cmlab.csie.ntu.edu.tw/status/', function (err, res, body) {
         deadMachines.push(machineId);
     }
 
-    // all machines are fine~
-    if (!deadMachines.length) return;
+    // All machines are fine~
+    if (!deadMachines.length && !$('.high').length) return;
 
-    // login to facebook and send msg to unix manager
-    login({
-        email: config.user_email,
-        password: config.user_password
-    }, function (err, api) {
+    // Screenshot
+    webshot(body, 'tmp.png', {
+        siteType: 'html',
+        captureSelector: 'table',
+        customCSS: css
+    }, function(err) {
         if (err) return console.error(err);
 
-        api.sendMessage(`${deadMachines.toString()} is dead.`, config.notify_channel_id, function (err) {
-            console.log(`${deadMachines.toString()} is dead.`);
+        // login to facebook and send msg to unix manager
+        login({
+            email: config.user_email,
+            password: config.user_password
+        }, function (err, api) {
+            if (err) return console.error(err);
+
+            const msg = {
+                body: '有狀況，請明察。',
+                attachment: fs.createReadStream(__dirname + '/tmp.png')
+            };
+
+            api.sendMessage(msg, config.notify_channel_id, function (err) {
+                console.log('done.');
+            });
         });
     });
 });
